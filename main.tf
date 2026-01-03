859b241d300672aab12fc0eb5f13e40e26e49848
Arun-KumarRavi <arunkumarravi0811@gmail.com>
2026-01-03 23:53:41 +0530

    Parameterize AWS region and S3 bucket name

--- main.tf
+++ main.tf
@@ -16,9 +16,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "terraform-cloud-s3-369852"
+  bucket = var.bucket_name
 }
